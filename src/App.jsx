00bc8505a1180ef6f98cc03b8d1fb6549c7a3103
setiyawt04 <tiyaland3@gmail.com>
2025-06-17 15:05:22 +0700

    create home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/Home"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <p className='text-2xl text-amber-300'>Hello</p>
+      <Home />
     </>
   )
 }
